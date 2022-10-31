2442f1dbc011b743bd3de404be43a3c724997e2e
Ayrene <112762086+ayreneYS@users.noreply.github.com>
2022-10-31 23:50:02 +0800

    index.js

--- src/index.js
+++ src/index.js
@@ -4,35 +4,13 @@ import { createRoot } from 'react-dom/client';
 // CSS
 import './index.css';
 
-/*
+// Books
+import { books } from './books';
+import Book from './Book';
+
 // ReactDOM.render(<Greeting />, document.getElementById('root'));
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<Greeting />);
-*/
 
 // setup vars {
-const books = [
-  {
-    id: 1,
-    img: 'https://m.media-amazon.com/images/I/41ZLnc34EiL._AC_UY218_.jpg',
-    title: 'Verity',
-    author: 'Colleen Hoover',
-  },
-
-  {
-    id: 2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71k+gTdHTOL._AC_UL210_SR195,210_.jpg',
-    title: 'The Passenger',
-    author: 'Cormac McCarthy',
-  },
-  {
-    id: 3,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81PMOtoT7zL._AC_UL210_SR195,210_.jpg',
-    title: 'Fairy Tale',
-    author: 'Stephen King',
-  },
-];
 
 function BookList() {
   return (
@@ -43,17 +21,6 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
-  return (
-    <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 // ReactDOM.render(<BookList />, document.getElementById('root'));
 
