fb936c1ed6a818a45195ca086da1f08904614313
Ayrene <112762086+ayreneYS@users.noreply.github.com>
2022-10-31 23:48:50 +0800

    JS datas

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/41ZLnc34EiL._AC_UY218_.jpg',
+    title: 'Verity',
+    author: 'Colleen Hoover',
+  },
+
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71k+gTdHTOL._AC_UL210_SR195,210_.jpg',
+    title: 'The Passenger',
+    author: 'Cormac McCarthy',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81PMOtoT7zL._AC_UL210_SR195,210_.jpg',
+    title: 'Fairy Tale',
+    author: 'Stephen King',
+  },
+];
